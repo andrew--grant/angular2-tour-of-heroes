dd55b688dc32393ec769f557c984a823e27ab7fd
Andrew Grant <agrant@gme.net.au>
2016-07-07 10:57:27 +1000

    http complete

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,24 +4,29 @@ import 'rxjs/add/operator/toPromise';
 import { Hero } from './hero';
 @Injectable()
 export class HeroService {
+
   private heroesUrl = 'app/heroes';  // URL to web api
   constructor(private http: Http) { }
+
   getHeroes(): Promise<Hero[]> {
     return this.http.get(this.heroesUrl)
       .toPromise()
       .then(response => response.json().data)
       .catch(this.handleError);
   }
+
   getHero(id: number) {
     return this.getHeroes()
       .then(heroes => heroes.find(hero => hero.id === id));
   }
+
   save(hero: Hero): Promise<Hero> {
     if (hero.id) {
       return this.put(hero);
     }
     return this.post(hero);
   }
+
   delete(hero: Hero) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
@@ -31,6 +36,7 @@ export class HeroService {
       .toPromise()
       .catch(this.handleError);
   }
+
   // Add new Hero
   private post(hero: Hero): Promise<Hero> {
     let headers = new Headers({
@@ -42,6 +48,7 @@ export class HeroService {
       .then(res => res.json().data)
       .catch(this.handleError);
   }
+
   // Update existing Hero
   private put(hero: Hero) {
     let headers = new Headers();
@@ -53,10 +60,10 @@ export class HeroService {
       .then(() => hero)
       .catch(this.handleError);
   }
+
   private handleError(error: any) {
     console.error('An error occurred', error);
     return Promise.reject(error.message || error);
   }
+
 }
-
-
